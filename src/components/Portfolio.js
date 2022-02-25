747e88fb57c705d93baf0786c0af54e7d1c9ea17
brandon.sauer <brantheman8@comcast.net>
2022-02-25 15:48:19 -0800

    added project and portfolio templates

---
+++ src/components/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Project from "./Project";
+
+function Portfolio() {
+  return (
+    <div>
+      <p className="content is-medium">Portfolio</p>
+      <hr />
+
+      <Project></Project>
+    </div>
+  );
+}
+
+export default Portfolio;
